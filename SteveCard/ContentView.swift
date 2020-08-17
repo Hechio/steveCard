31796ad8621778b4da82c417d59cc5a011b02625
Steve Hechio <stevehechio@gmail.com>
2020-08-17 21:08:01 +0300

    Initial Commit

---
+++ SteveCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SteveCard
+//
+//  Created by Joel Personal on 8/17/20.
+//  Copyright © 2020 Steve Hechio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
